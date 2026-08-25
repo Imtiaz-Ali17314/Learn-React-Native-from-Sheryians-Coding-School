8328d3105703e9c520fd57d15a19e967347941da
Imtiaz Ali <imtiazali17314@gmail.com>
2026-08-25 14:37:52 +0500

    Understand View and Text Components. View component is to wrap app other components and Text component is to show text on UI.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,13 @@
-import { Text, View, StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function Index() {
+const index = () => {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text>Hello World</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default index;
+
+const styles = StyleSheet.create({});
