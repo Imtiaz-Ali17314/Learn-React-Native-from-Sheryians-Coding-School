dbbd2a9dcdf5c48a38e8e67eb6b466f8ac7f7ff7
Imtiaz Ali <imtiazali17314@gmail.com>
2026-08-25 14:45:03 +0500

    Understand how to use Image component? how to use image in an App.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,16 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 const index = () => {
   return (
     <View>
       <Text>Hello World</Text>
+
+      <Image
+        source={{
+          uri: "https://images.unsplash.com/photo-1519086588705-c935fdedcc14?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3MDM0MTR8MHwxfHNlYXJjaHwyfHxhZXN0aGV0aWMlMjB3b3Jrc3BhY2V8ZW58MHx8fHwxNzg3NjUwNzI5fDA&ixlib=rb-4.1.0&q=80&w=1080",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
     </View>
   );
 };
